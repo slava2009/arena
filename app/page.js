479970345ecb04b392c754faa71e6b10112024f7
slava2009 <43423776+slava2009@users.noreply.github.com>
2023-08-09 19:56:57 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,12 +6,12 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Расписание ледовой арены
+          Расписание ледовой арены;
           <code className={styles.code}>Озерки</code>
         </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://katok-ozerki.ru/"
             target="_blank"
             rel="noopener noreferrer"
           >
