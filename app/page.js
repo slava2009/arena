6365d720410b16ecd87dbacf0125a0ca3056769c
slava2009 <43423776+slava2009@users.noreply.github.com>
2023-08-09 21:08:38 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Наш сайт{' '}
+            {' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
